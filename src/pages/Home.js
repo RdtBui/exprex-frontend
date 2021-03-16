ddc5dccefb6dd40c4eac65684417f3025ebf081d
rdtbui <richard.bui@member.mensa.org>
2021-03-16 15:14:16 -0400

    Added sections in home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,35 @@
 
 const Home = () => {
     return (
-        <h2>Home Page</h2>
+        <div class="col no-gutters">
+            <div class="row no-gutters">
+                <div class="section1 d-flex justify-content-center align-items-center">
+                    <h1>Home Page</h1>
+                    Section 1
+                </div>
+            </div>
+            <div class="row no-gutters">
+                <div class="section2 d-flex justify-content-center align-items-center">
+                    Section 2
+                </div>
+            </div>
+            <div class="row no-gutters">
+                <div class="section3 d-flex justify-content-center align-items-center">
+                    Section 3
+                </div>
+            </div>
+            <div class="row no-gutters">
+                <div class="section4 d-flex justify-content-center align-items-center">
+                    Section 4
+                </div>
+            </div>
+            <div class="row no-gutters">
+                <div class="section5 d-flex justify-content-center align-items-center">
+                    Section 5
+                </div>
+            </div>
+
+        </div>
     )
 }
 
