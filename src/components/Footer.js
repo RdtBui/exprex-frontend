f199f86cda789c0cb331e5c62e9c9b040ed1e03b
rdtbui <richard.bui@member.mensa.org>
2021-03-15 21:09:20 -0400

    Add Button, Footer, and Header basic script

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+    return (
+        <footer className='footer'>
+            <p>Bye</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { useLocation } from 'react-router-dom'
+
+const Header = () => {
+    // Logo, Nav, div
+    return (
+        <header className='header container-full'>
+
+        </header>
+    )
+}
+
+export default Header;
