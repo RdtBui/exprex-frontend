ec220a8e8c8c4611fd8f4c39366c817fb363c0d0
rdtbui <richard.bui@member.mensa.org>
2021-03-15 21:48:54 -0400

    Add logo svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { useLocation } from 'react-router-dom'
+import Logo from '../assets/logo.svg'
+
 
 const Header = () => {
     // Logo, Nav, div
     return (
         <header className='header container-full'>
+            <img alt="Logo" src={Logo} />
 
         </header>
     )
